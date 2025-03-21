09c7b62ff505d6676558b1cd2d02cd58d7c2afb4
WonJin <try3982@kakao.com>
2025-03-21 14:32:41 +0900

    회원 서비스 개발

---
+++ src/main/java/com/bwj/hellospring/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.bwj.hellospring.service;
+
+import com.bwj.hellospring.domain.Member;
+import com.bwj.hellospring.repository.MemberRepository;
+import com.bwj.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 증복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
