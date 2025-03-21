ce139c7d82fbae3fff306228bbadc4864eadb526
WonJin <try3982@kakao.com>
2025-03-21 13:46:47 +0900

    회원 도메인과 레포지토리 만들기

--- src/main/java/com/bwj/hellospring/controller/HelloController.java
+++ src/main/java/com/bwj/hellospring/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.bwj.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,5 +16,28 @@ public class HelloController {
         model.addAttribute("data", "Hello");
         return "hello";
 
+    }
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 body에 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
